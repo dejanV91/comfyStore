276fbd6237d7ee98c2998ff8419c2860e60fac56
dejanV91 <dejanvukoicic91@gmail.com>
2023-02-18 09:47:59 +0100

    Setup theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ThemeProvider } from "@mui/material";
 
 function App() {}
 
---
+++ src/components/ui/Theme.js
@@ -0,0 +1,7 @@
+import { createTheme } from "@mui/material";
+
+export default theme = createTheme({
+  palette: {
+    primary: "#DFC472",
+  },
+});
