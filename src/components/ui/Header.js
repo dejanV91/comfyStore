c4e75abe350fe743252b8e34db60637b902e9a84
dejanV91 <dejanvukoicic91@gmail.com>
2023-02-19 21:57:42 +0100

    WIP AppBar (cart and login button)

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,18 +1,56 @@
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 import { makeStyles } from "tss-react/mui";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
 import React from "react";
+
+import logo from "../../images/comfy.jpg";
 
 const useStyles = makeStyles()((theme) => ({
   appBarStyle: {
     backgroundColor: "#ffffff",
     boxShadow: "none",
+    alignItems: "center",
+  },
+  toolbarDiv: {
+    width: "1170px",
+  },
+  gridContainer: {
+    alignItems: "center",
+  },
+  logoIcon: {
+    maxHeight: "4em",
   },
 }));
 const Header = () => {
   const { classes } = useStyles();
   return (
     <AppBar className={classes.appBarStyle} position="static">
-      <Toolbar></Toolbar>
+      <Toolbar className={classes.toolbarDiv}>
+        <Grid
+          container
+          justifyContent="space-between"
+          className={classes.gridContainer}
+        >
+          <Grid item>
+            <Link href="/">
+              <img className={classes.logoIcon} src={logo} alt="logo" />
+            </Link>
+          </Grid>
+          <Grid item>
+            <Button variant="text">
+              <Typography>Home</Typography>
+            </Button>
+            <Button variant="text">
+              <Typography>About</Typography>
+            </Button>
+            <Button variant="text">
+              <Typography>Products</Typography>
+            </Button>
+          </Grid>
+          <Grid item></Grid>
+        </Grid>
+      </Toolbar>
     </AppBar>
   );
 };
