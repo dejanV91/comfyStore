59fe8ba823fc715d27d6d7d7bba32cbf05941a4c
dejanV91 <dejanvukoicic91@gmail.com>
2023-03-20 10:49:15 +0100

    WIP header

---
+++ src/components/common/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-div">
+      <div className="header-container">
+        <h1>dejan</h1>
+        <h1>dejan</h1>
+        <h1>dejan</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
