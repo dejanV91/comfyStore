83d003567345d430002017d5b3bfba4416436a69
dejanV91 <dejanvukoicic91@gmail.com>
2023-04-06 12:38:27 +0200

    setup project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <div>
+      <h4>comfy sloth starter</h4>
+    </div>
+  );
 }
 
-export default App
+export default App;
