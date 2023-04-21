8605d270052a855bd7cc27b773724a7a4383c8d8
dejanV91 <dejanvukoicic91@gmail.com>
2023-04-21 12:54:55 +0200

    finished product image

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -24,7 +24,6 @@ const SingleProductPage = () => {
     single_product: product,
     fetchSingleProduct,
   } = useProductsContext();
-  console.log(product);
 
   useEffect(() => {
     fetchSingleProduct(`${url}${id}`);
@@ -63,8 +62,8 @@ const SingleProductPage = () => {
         <Link to="/products" className="btn">
           back to products
         </Link>
-        <div className="products-center">
-          <ProductImages />
+        <div className="product-center">
+          <ProductImages images={images} />
           <section className="content">
             <h2>{name}</h2>
             <Stars />
